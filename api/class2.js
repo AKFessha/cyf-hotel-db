068be17ab12cc852ba3fbbd7d4dc8ace01e06e57
Awet Fessha <awetkifle94@gmail.com>
2018-07-22 22:28:48 +0100

    selecting all customers

--- api/class2.js
+++ api/class2.js
@@ -7,60 +7,14 @@ const knex = require("knex")({
     filename
   }
 });
+
+//Get customers
 router.get("/customers", function(req, res) {
   const sqlStatement = "select * from customers";
   knex.raw(sqlStatement).then(function(data) {
     res.json(data);
   });
   res.status(200);
-});
-
-router.get("/customers/:id", function(req, res) {
-  const sqlStatement = `select * from customers where id=${req.params.id}`;
-  knex.raw(sqlStatement).then(function(data) {
-    res.json(data);
-  });
-});
-
-router.get("/customers/:surname", function(req, res) {
-  const sqlStatement = `select * from customers where surname=${
-    req.params.surname
-  }`;
-  knex.raw(sqlStatement).then(function(data) {
-    res.json(data);
-  });
-});
-
-router.delete("/customers/:id", function(req, res) {
-  const sqlStatement = `delete * from customers where id=${req.params.id}`;
-  knex.raw(sqlStatement).then(function(data) {
-    res.json(data);
-  });
-});
-
-router.post("/customers/", function(req, res) {
-  const body = req.body;
-
-  const sqlStatement = `INSERT INTO customers (title, firstname, surname, email) VALUES ("${
-    body.title
-  }", "${body.firstname}", "${body.surname}","${body.email}")`;
-  knex.raw(sqlStatement).then(function(data) {
-    res.json(data);
-  });
-  res.send("Success");
-});
-
-router.put("/customers/:id", function(req, res) {
-  const body = req.body;
-  const customerId = req.params.id;
-  const sqlStatement = `UPDATE customers 
-  SET title =" ${body.title}",
-   firstname ="${body.firstname}"
-  WHERE id =${customerId}`;
-  knex.raw(sqlStatement).then(function(data) {
-    res.json(data);
-  });
-  res.send("Success");
 });
 
 // get '/reservations'
